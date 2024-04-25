0230e52012a8c250bc7e5581381a9ccc214d4ea0
Sergio Dzhi <sergio.dzhioev@yandex.ru>
2024-04-25 23:19:06 +0300

    test

---
+++ script.js
@@ -0,0 +1,30 @@
+
+
+
+
+
+
+var init = false;
+var swiper;
+function swiperCard() {
+  if (window.innerWidth <= 767) {
+    if (!init) {
+      init = true;
+      swiper = new Swiper(".swiper", {
+        direction: "horizontal",
+        slidesPerView: "auto",
+        centeredSlides: true,
+        spaceBetween: 32,
+        pagination: {
+          el: ".swiper-pagination",
+          clickable: true,
+        },
+      });
+    }
+  } else if (init) {
+    swiper.destroy();
+    init = false;
+  }
+}
+swiperCard();
+window.addEventListener("resize", swiperCard);
