be4147058254892da8d29ae2333fbc682f573a96
Sergio Dzhi <sergio.dzhioev@yandex.ru>
2024-04-26 22:47:31 +0300

    test

--- script.js
+++ script.js
@@ -1,8 +1,25 @@
+const showBtn = document.querySelector(".change__btn");
+const cardContainer = document.querySelector(".card__container");
+const cardItem = cardContainer.querySelectorAll(".card__item");
 
+for (let i = 6; i < cardItem.length; i++) {
+  cardItem[i].classList.remove("hidden");
+  toggleBtn(cardItem[i]);
+}
 
-
-
-
+function toggleBtn(item) {
+  showBtn.addEventListener("click", function () {
+    if (item.classList.contains("hidden")) {
+      showBtn.textContent = "Скрыть";
+      showBtn.classList.add("revert");
+      item.classList.remove("hidden");
+    } else {
+      showBtn.textContent = "Показать все";
+      showBtn.classList.remove("revert");
+      item.classList.add("hidden");
+    }
+  });
+}
 
 var init = false;
 var swiper;
